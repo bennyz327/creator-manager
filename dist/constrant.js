b9ccbbdf6423d60a4262c9aa6fd22c6f4c00e762
Benny Chou <bennyz327@gmail.com>
2024-07-27 23:07:42 +0800

    remove dist

--- dist/constrant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dbName = 'creators.db';
-exports.default = {
-    DB_NAME: dbName
-};
-//# sourceMappingURL=constrant.js.map
